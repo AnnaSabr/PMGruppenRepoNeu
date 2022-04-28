9a1a8caed91124dbee200a35e4d5a73b66edf183
lterlutter <leonie.terlutter@fh-bielefeld.de>
2022-04-29 00:21:25 +0200

    Item kann gesammelt werden

--- dungeon-starter/code/desktop/src/desktop/SpeedPotion.java
+++ dungeon-starter/code/desktop/src/desktop/SpeedPotion.java
@@ -3,8 +3,11 @@ package desktop;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import graphic.Painter;
 import level.elements.Level;
+import level.elements.room.Tile;
 import level.tools.Coordinate;
 import tools.Point;
+
+import java.util.List;
 
 public class SpeedPotion extends Potion{
 
@@ -12,7 +15,7 @@ public class SpeedPotion extends Potion{
 
     String texturePath;
     Point position;
-    public static float SpeedIncrease = PotionsTaken*0.2f;
+    public static float SpeedIncrease = PotionsTaken*0.5f;
     private Point point ;
 
     /**
@@ -27,10 +30,12 @@ public class SpeedPotion extends Potion{
         texturePath = "character/items/flask_big_blue.png";
     }
 
+
     public void setLevel(Level level) {
         Level currentLevel = level;
-        position=level.getStartTile().getCoordinate().toPoint();
-
+        //position=level.getStartTile().getCoordinate().toPoint();
+        Coordinate c = new Coordinate(-1,0);
+        position=c.toPoint();
     }
 
     @Override
@@ -48,5 +53,21 @@ public class SpeedPotion extends Potion{
     @Override
     public String getTexturePath() {
         return texturePath;
+    }
+
+    public boolean removable() {
+        float x = MyHero.position.x;
+        float y = MyHero.position.y;
+
+        int coordx = (int) getPosition().x;
+        int coordy = (int) getPosition().y;
+
+        if(x>coordx-1&&x<coordx+1) {
+            if (y > coordy - 1 && y < coordy + 1) {
+                usePotion();
+                return true;
+            }
+        }
+        return false;
     }
 }
