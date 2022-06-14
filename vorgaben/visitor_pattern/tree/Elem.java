89e578084b69e4aeb5f9c925485fd69c97f67fde
lterlutter <leonie.terlutter@fh-bielefeld.de>
2022-06-14 15:57:12 +0200

    Kommentare ergänzt

--- vorgaben/visitor_pattern/tree/Elem.java
+++ vorgaben/visitor_pattern/tree/Elem.java
@@ -2,5 +2,10 @@ package tree;
 
 public interface Elem {
 
+    /**
+     * ruft visit auf dem Visitor Parameter auf
+     *
+     * @param visitor
+     */
     void accept(Visitor visitor);
 }
--- vorgaben/visitor_pattern/tree/Visitor.java
+++ vorgaben/visitor_pattern/tree/Visitor.java
@@ -2,5 +2,10 @@ package tree;
 
 public interface Visitor {
 
+    /**
+     * Methode um eine Operation auf der Node durchzuführen
+     *
+     * @param node
+     */
     void visit(Node node);
 }
